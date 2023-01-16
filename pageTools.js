ba85c450d73eb39a228dacc5a61217be8c5e752c
Riloo007 <107699833+Riloo007@users.noreply.github.com>
2023-01-16 13:49:45 -0700

    Started the deep clean

--- pageTools.js
+++ pageTools.js
@@ -55,6 +55,61 @@ function generateMathHtml(equation) {
     return finalString;
 }
 
+function rInt(i) {
+  return Math.round(Math.random() * i);
+}
+
+function pad(num) {
+  num = num.toString();
+  while (num.length < 2) num = "0" + num;
+  return num;
+}
+
+var database;
+function setupFirebase() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBD8TGQSt2Oit3A0Ow4vqUbi9iSarjAhng",
+    authDomain: "chsphysicstoolbox.firebaseapp.com",
+    projectId: "chsphysicstoolbox",
+    storageBucket: "chsphysicstoolbox.appspot.com",
+    messagingSenderId: "1013873625231",
+    appId: "1:1013873625231:web:9e179abf423b8891f2c8fa",
+    measurementId: "G-7Z6SQHBGPV"
+  };
+  firebase.initializeApp(firebaseConfig);
+  database = firebase.database();
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  // Header and Footer
+  header = `
+    <p style="font-size: 20px; margin: 3px;">
+      chsphysicstoolbox
+    </p>
+  `;
+
+  footer = `
+  <div>
+    <p>
+        Copyright 2023 Glitch
+    </p>
+    <p id="FooterMessage">
+    </p>
+  </div>
+  `
+
+  document.getElementById('header').innerHTML = header;
+  document.getElementById('footer').innerHTML = footer;
+
+  if(!database) {setupFirebase();}
+  const ref = database.ref('messages/' + pad(rInt(2)));
+  ref.on('value', function(snapshot){
+    snapshot.val();
+    document.getElementById("FooterMessage").innerHTML = snapshot.val();
+  });
+});
+
+
 
 /*
 //
