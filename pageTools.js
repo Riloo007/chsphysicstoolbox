044e77cff8c0973f3b05cb1dac65322e9b9856e9
Riloo007 <107699833+Riloo007@users.noreply.github.com>
2023-01-18 18:16:56 -0700

    YESS
    generateMathHTML works now
    ... Except sqrt()...

--- pageTools.js
+++ pageTools.js
@@ -3,6 +3,7 @@ function generateMathHtml(equation) {
     var tokens = equation.split("");
     finalString += "<table class='Formula'><tbody><tr><td>";
     var inExponent = 0;
+    var inSub = 0;
     // Cycle through each character
     for (var i = 0; i < tokens.length; i++) {
         var token = tokens[i];
@@ -10,17 +11,53 @@ function generateMathHtml(equation) {
         // Exponents
         if (token == "^" && tokens[i+1] == "(") {
           finalString += "<sup>"
-          inExponent += 1;
+          inExponent++;
           i+=1;
         }
-        else if (token == ")" && inExponent > 0) {
-          inExponent--;
-          finalString += "</sup>";
-          i+=1;
+        else if (token == "v" && tokens[i+1] == "e" && tokens[i+2] == "c" && tokens[i+3] == "(") {
+          finalString += "<span class='vec'>" + tokens[i+4] + "</span>";
+          i+=5;
+        }
+        else if (token == "s" && tokens[i+1] == "u" && tokens[i+2] == "b" && tokens[i+3] == "(") {
+          finalString += "<sub>";
+          inSub++;
+          i+=3;
+        }
+        else if (token == ")") {
+          if(inExponent > 0) {
+            inExponent--;
+            finalString += "</sup>";
+            i+=1;
+          } else if (inSub > 0) {
+            inSub--;
+            finalString += "</sub>";
+          }
         }
 
         // Fractions
         else if (token == "[") {
+          var off = 0;
+          while(tokens[i + off] != "]" && tokens[i + off] != "undefined") {
+            off++;
+          }
+          console.log(off, "[SPACES UNTIL FRACTION SPLITS]");
+          console.log("[RESULTING STRING]: ", equation.substr(i+1, off-1));
+          finalString += "</td><td><div><table><tbody><tr><td>" + generateMathHtml(equation.substr(i+1, off-1));
+          finalString += "</td></tr><tr><td><div style='border-top: 10px solid #FFFFFF;'></div></td></tr><tr><td>";
+          off += 3;
+          var offp = off;
+          while(tokens[i + offp] != "]" && tokens[i + offp] != "undefined") {
+            offp++;
+          }
+          console.log(offp - off, "[SPACES UNTIL FRACTION ENDS]");
+          console.log("[RESULTING STRING]: ", equation.substr(i + off, offp - off));
+          finalString += generateMathHtml(equation.substr(i + off, offp - off)) + "</td></tr></tbody></table></div></td><td>";
+          i += offp;
+          /*return "ERR";
+
+          + "</td></tr></tbody></table></div></td><td>";
+
+
           // Problem: Fraction takes over and doesn't style exponents in the fraction
           finalString += "</td>";
           fractionIsntFinished = true;
@@ -41,7 +78,7 @@ function generateMathHtml(equation) {
             // Fraction Contents
               finalString += token;
             }
-          }
+          } */
         }
         
         // Add anything else
@@ -103,6 +140,7 @@ document.addEventListener('DOMContentLoaded', function(){
         Copyright 2023 Glitch
     </p>
     <p id="FooterMessage">
+      If you see this message, then something is very wrong! Try connecting to a network or reloading the page.
     </p>
   </div>
   `
